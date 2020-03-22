2eb6d0d281e08361d207f279006dd8aa9a3e6bfd
Akachukwu Udeh <akaudeh@gmail.com>
2020-03-22 22:52:35 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var fullname = 'Akachukwu Udeh';
+
+var courses = ['HTML', 'CSS', 'JavaScript'];
+
+console.log(fullname, courses);
+
+for(var i = 0; i <= 200; i++) {
+    if (i % 2 !== 0) console.log(i);
+}
